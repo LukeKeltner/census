5a32f6cf87f7027c90b5dcd139f6aa76ff63f631
Luke Keltner <lkeltner87@gmail.com>
2018-01-14 21:37:26 -0500

    playing with api

--- app/public/assets/scripts/index.js
+++ app/public/assets/scripts/index.js
@@ -1,1 +1,10 @@
-console.log("hellllllo!")
+var key = "78cc287ce36b44676e357af5c8fd8569925c437d";
+
+$.ajax(
+{
+	url: "https://api.census.gov/data/2016/acs/acs5?get=NAME,B01001_001E&for=county:035&in=state:39&key="+key,
+	type: "GET"
+}).done(function(result)
+{
+	console.log(result)
+})
